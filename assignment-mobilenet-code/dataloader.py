95715c6c42673125f0248f69eeb6a996e3b326f6
chenxr35 <2649167877@qq.com>
2023-10-22 15:37:49 +0800

    update dataloader file

--- assignment-mobilenet-code/dataloader.py
+++ assignment-mobilenet-code/dataloader.py
@@ -4,87 +4,56 @@ import torch
 import torch.utils.data as data
 from torchvision import datasets
 import torchvision.transforms as transforms
-
-
-class CustomDataset(data.Dataset):
-    channel_mean = np.random.random((3))
-    channel_std = np.random.random((3))
-
-    def __init__(self, train_dataset, idxs, augment):
-
-        normalize = transforms.Normalize(
-            mean=list(CustomDataset.channel_mean),
-            std=list(CustomDataset.channel_std),
-        )
-
-        if augment:
-            transform = transforms.Compose([
-                transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                normalize,
-            ])
-        
-        else:
-            transform = transforms.Compose([
-                    transforms.ToTensor(),
-                    normalize,
-            ])
-
-        self.data = [transform(Image.fromarray(np.uint8(train_dataset.data[idx])).convert('RGB')) for idx in idxs]
-        self.targets = np.array(train_dataset.targets)[idxs]
-      
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.targets[idx]
-
-    def class_proportion(self, cls):
-        return len(np.where(self.targets==cls)[0])/len(self.targets)
-
-
-def random_train_val_split(train_dataset, valid_size, random_seed):
-    num_train = len(train_dataset)
-    indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
-    np.random.seed(random_seed)
-    np.random.shuffle(indices)
-    train_idx, valid_idx = indices[split:], indices[:split]
-    return train_idx, valid_idx
-
-
-def calculate_mean_std(train_dataset, train_idx):
-    train_data = np.array(train_dataset.data)[train_idx]
-    N, H, W, C = train_data.shape[:4]
-    train_data = train_data.reshape((N, C, -1))
-    train_data = train_data / 255
-    channel_mean = np.sum(np.mean(train_data, axis=2), axis=0) / N
-    channel_std = np.sum(np.std(train_data, axis=2), axis=0) / N
-    return channel_mean, channel_std
+from torch.utils.data.sampler import SubsetRandomSampler
 
 
 def get_train_valid_loader(dataset_dir, batch_size, augment, random_seed, save_images):
-    train_dataset = datasets.cifar.CIFAR100(root='cifar100', train=True, transform=None, download=False)
 
-    train_idx, valid_idx = random_train_val_split(train_dataset, 0.2, random_seed)
+    normalize = transforms.Normalize(
+        mean=[0.5179, 0.4660, 0.4579],
+        std=[0.1884, 0.2026, 0.1855],
+    )
 
-    channel_mean, channel_std = calculate_mean_std(train_dataset, train_idx)
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ])
 
-    CustomDataset.channel_mean = channel_mean
-    CustomDataset.channel_std = channel_std
+    valid_transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
 
-    train_set = CustomDataset(train_dataset, train_idx, augment)
+    train_dataset = datasets.cifar.CIFAR100(
+        root='cifar100', train=True,
+        transform=train_transform, download=True
+    )
 
-    valid_set = CustomDataset(train_dataset, valid_idx, False)
+    valid_dataset = datasets.cifar.CIFAR100(
+        root='cifar100', train=True,
+        transform=valid_transform, download=True
+    )
+
+    valid_size = 0.2
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
 
     train_loader = torch.utils.data.DataLoader(
-        train_set, batch_size=batch_size,
+        train_dataset, batch_size=batch_size,
+        sampler=train_sampler,
         num_workers=2, pin_memory=True,
     )
 
     valid_loader = torch.utils.data.DataLoader(
-        valid_set, batch_size=batch_size, 
+        valid_dataset, batch_size=batch_size,
+        sampler=valid_sampler,
         num_workers=2, pin_memory=True,
     )
 
@@ -92,15 +61,24 @@ def get_train_valid_loader(dataset_dir, batch_size, augment, random_seed, save_i
 
 
 def get_test_loader(dataset_dir, batch_size):
+
+    normalize = transforms.Normalize(
+        mean=[0.5179, 0.4660, 0.4579],
+        std=[0.1884, 0.2026, 0.1855],
+    )
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
     
-    test_dataset = datasets.cifar.CIFAR100(root='cifar100', train=False, transform=None, download=False)
-
-    test_idx = [i for i in range(len(test_dataset))]
-
-    test_set = CustomDataset(test_dataset, test_idx, False)
+    test_dataset = datasets.cifar.CIFAR100(
+        root='cifar100', train=False,
+        transform=test_transform, download=True
+    )
 
     test_loader = torch.utils.data.DataLoader(
-        test_set, batch_size=batch_size, 
+        test_dataset, batch_size=batch_size,
         num_workers=2, pin_memory=True,
     )
 
